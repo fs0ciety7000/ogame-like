141740385ef9ebef2fa0331e2c7e4375c40ad133
phant0mhex <nd.managment@gmail.com>
2026-09-20 19:09:02 +0200

    Revert: retour à l'état du commit 1a2e622

--- src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { startAuthListener, useAuthStore } from "@/store/authStore";
-
-export function useAuth() {
-  useEffect(() => {
-    startAuthListener();
-  }, []);
-  return useAuthStore();
-}
